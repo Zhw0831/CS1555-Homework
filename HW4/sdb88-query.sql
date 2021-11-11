a108a9fdfb53a8ef1f70e005f99f9bed08aa786e
Zhw0831 <61070056+Zhw0831@users.noreply.github.com>
2021-11-11 01:38:55 -0500

    Update sdb88-query.sql

--- HW4/sdb88-query.sql
+++ HW4/sdb88-query.sql
@@ -28,17 +28,23 @@ FROM WORKER;
 
 -------- Question 2
 --(a)
+-- note: road_no is primary key, which is NOT DEFERRED in our case. So it cannot be changed during execution.
+BEGIN;
+SET CONSTRAINTS ALL DEFERRED;
 INSERT INTO ROAD VALUES (105,'Route Five',426);
 INSERT INTO INTERSECTION VALUES (1, 105);
+COMMIT;
 
 --(b)
-begin;
+-- the initial constraint for maintainer is immediate deferrable. I think it's fine to keep it as immediate.
+BEGIN;
 UPDATE SENSOR
 SET maintainer = 'xxxxxxxxx'
 WHERE maintainer = (SELECT w.ssn
                   FROM WORKER w
                   WHERE w.name = 'Jason');
-
+COMMIT;
+BEGIN;
 UPDATE SENSOR
 SET maintainer = (SELECT w.ssn
                   FROM WORKER w
@@ -46,22 +52,22 @@ SET maintainer = (SELECT w.ssn
 WHERE maintainer = (SELECT w.ssn
                   FROM WORKER w
                   WHERE w.name = 'John');
-
+COMMIT;
+BEGIN;
 UPDATE SENSOR
 SET maintainer = (SELECT w.ssn
                   FROM WORKER w
                   WHERE w.name = 'John')
 WHERE maintainer = 'xxxxxxxxx';
-end;
-
+COMMIT;
 --(c)
--- begin end?
+BEGIN;
+SET CONSTRAINTS ALL DEFERRED;
 INSERT INTO WORKER VALUES ('105588973', 'Natalia', 1, 'OH');
 UPDATE SENSOR
-SET maintainer = (SELECT w.ssn
-                  FROM WORKER w
-                  WHERE w.name = 'Natalia')
+SET maintainer = '105588973'
 WHERE sensor_id = 2;
+COMMIT;
 
 -------- Question 3
 --(a)
@@ -118,17 +124,33 @@ WHERE f.forest_no IN (
     ) AND acid_level > 0.6;
 
 --(g)
-SELECT *
-FROM INTERSECTION i RIGHT OUTER JOIN(
-         SELECT i2.road_no
-         FROM FOREST f2 JOIN INTERSECTION i2 on f2.forest_no = i2.forest_no
-         WHERE f2.name = 'Big Woods') AS i3 on i.road_no=i3.road_no
-WHERE i3.road_no IS NOT NULL;
+SELECT f.name
+FROM FOREST f NATURAL JOIN (
+SELECT forest_no
+FROM (SELECT forest_no
+      FROM INTERSECTION i
+      WHERE i.road_no IN (
+        SELECT i1.road_no
+        FROM FOREST f1 JOIN INTERSECTION i1 on f1.forest_no = i1.forest_no
+        WHERE f1.name = 'Big Woods')) AS "extracted",
+        (SELECT COUNT(*) as count
+         FROM (SELECT i2.road_no
+        FROM FOREST f2 JOIN INTERSECTION i2 on f2.forest_no = i2.forest_no
+        WHERE f2.name = 'Big Woods') AS "bigwoods") AS "bigwoods_count"
+WHERE forest_no <> (SELECT f3.forest_no
+                    FROM FOREST f3
+                    WHERE f3.name = 'Big Woods')
+GROUP BY forest_no, bigwoods_count.count
+HAVING COUNT(forest_no) = bigwoods_count.count) AS "final_forest_no";
+
 
 
 --(h)
 --SELECT
---FROM REPORT
+--FROM REPORT LEFT OUTER JOIN
+        --(SELECT i1.road_no
+        --FROM FOREST f1 JOIN INTERSECTION i1 on f1.forest_no = i1.forest_no
+        --WHERE f1.name = 'Big Woods') AS "big_woods" on compare.road_no = big_woods.road_no;
 --WHERE
 
 
@@ -151,14 +173,39 @@ WHERE maintainer IS NOT NULL
 GROUP BY maintainer;
 
 --(c)
+DROP VIEW IF EXISTS FOREST_SENSOR;
 CREATE OR REPLACE VIEW FOREST_SENSOR AS
 SELECT f.forest_no, f.name, s.sensor_id
 FROM FOREST f, SENSOR s
 WHERE s.x BETWEEN f.mbr_xmin AND f.mbr_xmax AND s.y BETWEEN f.mbr_ymin AND f.mbr_ymax;
 
 --(d)
+DROP VIEW IF EXISTS FOREST_ROAD;
 CREATE OR REPLACE VIEW FOREST_ROAD AS
-SELECT forest_no, COUNT(road_no)
+SELECT forest_no, COUNT(road_no) as count
 FROM INTERSECTION
 GROUP BY forest_no;
+
+
+-------- Question 5
+REFRESH MATERIALIZED VIEW DUTIES_MV;
+
+--(a)
+SELECT forest_no
+FROM ( SELECT forest_no, RANK() over (ORDER BY count DESC) as rank
+       FROM FOREST_ROAD) AS rank_view
+WHERE rank =2;
+
+--(b)
+SELECT maintainer, name, employing_state, area
+FROM COVERAGE c JOIN (
+SELECT maintainer, name, employing_state
+FROM (SELECT maintainer, RANK() OVER (ORDER BY num_sensors DESC) as rank
+      FROM DUTIES) AS workers
+     JOIN WORKER w on workers.maintainer = w.ssn
+WHERE workers.rank = 1) AS get_state on get_state.employing_state = c.state;
+
+--(c)
+
+
 
