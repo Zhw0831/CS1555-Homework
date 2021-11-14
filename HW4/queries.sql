6b5746f7ad1621243e37087a28a3cabefb521e8a
Sushruti Bansod <sdb88@pitt.edu>
2021-11-13 19:05:35 -0500

    5 done

--- HW4/queries.sql
+++ HW4/queries.sql
@@ -1,3 +1,7 @@
+--- Team #7:
+---     Zhen Wu (zhw87)
+---     Sushruti Bansod (sdb88)
+
 SELECT *
 FROM FOREST;
 
@@ -22,123 +26,193 @@ FROM REPORT;
 SELECT *
 FROM WORKER;
 
------------------------------------
----------QUESTION 2----------------
------------------------------------
----A
-INSERT INTO ROAD VALUES(105, 'Route Five', 426);
-INSERT INTO INTERSECTION VALUES(1, 105);
+-------- Question 2
+--(a)
+-- note: road_no is primary key, which is NOT DEFERRED in our case. So it cannot be changed during execution.
+BEGIN;
+SET CONSTRAINTS ALL DEFERRED;
+INSERT INTO ROAD VALUES (105,'Route Five',426);
+INSERT INTO INTERSECTION VALUES (1, 105);
+COMMIT;
 
----B
----I DO NOT UNDERSTAND
-UPDATE SENSOR SET maintainer = (SELECT W.ssn
-                                FROM WORKER AS W
-                                WHERE W.name = 'Jason') WHERE maintainer = (SELECT W.ssn
-                                                                            FROM WORKER AS W
-                                                                            WHERE W.name = 'John');
-UPDATE SENSOR SET maintainer = (SELECT W.ssn
-                                FROM WORKER AS W
-                                WHERE W.name = 'John') WHERE maintainer = (SELECT W.ssn
-                                                                            FROM WORKER AS W
-                                                                            WHERE W.name = 'Jason');
+--(b)
+-- the initial constraint for maintainer is immediate deferrable. I think it's fine to keep it as immediate.
+BEGIN;
+UPDATE SENSOR
+SET maintainer = 'xxxxxxxxx'
+WHERE maintainer = (SELECT w.ssn
+                  FROM WORKER w
+                  WHERE w.name = 'Jason');
+COMMIT;
+BEGIN;
+UPDATE SENSOR
+SET maintainer = (SELECT w.ssn
+                  FROM WORKER w
+                  WHERE w.name = 'Jason')
+WHERE maintainer = (SELECT w.ssn
+                  FROM WORKER w
+                  WHERE w.name = 'John');
+COMMIT;
+BEGIN;
+UPDATE SENSOR
+SET maintainer = (SELECT w.ssn
+                  FROM WORKER w
+                  WHERE w.name = 'John')
+WHERE maintainer = 'xxxxxxxxx';
+COMMIT;
+--(c)
+BEGIN;
+SET CONSTRAINTS ALL DEFERRED;
+INSERT INTO WORKER VALUES ('105588973', 'Natalia', 1, 'OH');
+UPDATE SENSOR
+SET maintainer = '105588973'
+WHERE sensor_id = 2;
+COMMIT;
 
----why did you do what you did?? cant we just overwrite them
----C
-INSERT INTO WORKER (ssn, name,  rank, employing_state) VALUES ('105588973','Natalia',1, 'OH');
-UPDATE SENSOR SET maintainer = '105588973' WHERE sensor_id = 2;
-
-
-
------------------------------------
----------QUESTION 3----------------
------------------------------------
-
-----A
-SELECT sensor_id, Count(*) FROM REPORT
+-------- Question 3
+--(a)
+SELECT sensor_id, count(report_time)
+FROM REPORT
 GROUP BY sensor_id
-HAVING COUNT(*) IS NOT NULL
-ORDER BY Count(*) DESC
+HAVING count(report_time) IS NOT NULL
+ORDER BY count(report_time) DESC
 FETCH FIRST 3 ROWS ONLY;
 
-----B
-SELECT sensor_id, Count(*) FROM REPORT
+--(b)
+SELECT sensor_id, count(report_time)
+FROM REPORT
 GROUP BY sensor_id
-HAVING COUNT(*) IS NOT NULL
-ORDER BY Count(*) DESC
+HAVING count(report_time) IS NOT NULL
+ORDER BY count(report_time) DESC
 FETCH FIRST 2 ROWS ONLY OFFSET 3;
 
-----C
-SELECT C.state, sum(C.area)
-FROM COVERAGE AS C
-GROUP BY C.state
-HAVING SUM(C.area) > (SELECT sum(C2.area)
-                        FROM COVERAGE AS C2 JOIN STATE S ON C2.state = S.abbreviation
-                        WHERE S.name = 'Pennsylvania')
--- ORDER BY sum(C.area) DESC;   why
+--(c)
+SELECT t1.state, sum(t1.area)
+FROM COVERAGE t1
+GROUP BY t1.state
+HAVING sum(t1.area) > (
+    SELECT sum(t2.area)
+    FROM COVERAGE t2 JOIN STATE t on t2.state = t.abbreviation
+    WHERE t.name='Pennsylvania'
+    )
+ORDER BY sum(t1.area) DESC;
 
-----D
-SELECT DISTINCT R.name
-FROM ROAD AS R JOIN INTERSECTION I ON I.road_no = R.road_no
-WHERE R.road_no IN (SELECT I2.road_no
-                    FROM FOREST F JOIN INTERSECTION I2 ON F.forest_no = I2.forest_no
-                    WHERE F.name = 'Stone Valley');
+--(d)
+SELECT DISTINCT r.name
+FROM ROAD r JOIN INTERSECTION i1 on r.road_no = i1.road_no
+WHERE r.road_no IN (
+    SELECT i2.road_no
+    FROM FOREST f JOIN INTERSECTION i2 on f.forest_no = i2.forest_no
+    WHERE f.name = 'Stone Valley'
+    );
 
-----E
-SELECT DISTINCT ssn, name, RANK() over (ORDER BY rank DESC) as rank
-FROM(
-    SELECT W.ssn, W.name, W.rank
-    FROM WORKER W JOIN SENSOR S ON W.ssn = S.maintainer
-    WHERE S.energy <= 2) as R;
+--(e)
+SELECT DISTINCT ssn, name, RANK() over (ORDER BY rank DESC) AS rank
+FROM (
+    SELECT w.ssn, w.name, w.rank
+    FROM WORKER w JOIN SENSOR s ON w.ssn = s.maintainer
+    WHERE s.energy <= 2
+    ) AS R;
 
-----F
-SELECT DISTINCT F.NAME
-FROM FOREST AS F JOIN COVERAGE C ON F.forest_no = C.forest_no
-WHERE F.forest_no IN (SELECT C2.forest_no
-                    FROM COVERAGE AS C2 JOIN STATE AS S ON C2.state = S.abbreviation
-                    WHERE S.name = 'Pennsylvania') AND F.acid_level > 0.6;
+--(f)
+SELECT DISTINCT f.name
+FROM FOREST f JOIN COVERAGE c1 on f.forest_no = c1.forest_no
+WHERE f.forest_no IN (
+    SELECT c2.forest_no
+    FROM COVERAGE c2 JOIN STATE t on c2.state = t.abbreviation
+    WHERE t.name = 'Pennsylvania'
+    ) AND acid_level > 0.6;
 
-----g
-SELECT DISTINCT F.name
-FROM FOREST F JOIN (INTERSECTION I RIGHT OUTER JOIN(
-                                    SELECT I2.road_no
-                                    FROM FOREST F2 JOIN INTERSECTION I2 on F2.forest_no = i2.forest_no
-                                    WHERE F2.name = 'Big Woods') AS T on I.road_no=T.road_no) ON F.forest_no = I.forest_no
-WHERE T.road_no IS NOT NULL;
+--(g)
+SELECT f.name
+FROM FOREST f NATURAL JOIN (
+SELECT forest_no
+FROM (SELECT forest_no
+      FROM INTERSECTION i
+      WHERE i.road_no IN (
+        SELECT i1.road_no
+        FROM FOREST f1 JOIN INTERSECTION i1 on f1.forest_no = i1.forest_no
+        WHERE f1.name = 'Big Woods')) AS "extracted",
+        (SELECT COUNT(*) as count
+         FROM (SELECT i2.road_no
+        FROM FOREST f2 JOIN INTERSECTION i2 on f2.forest_no = i2.forest_no
+        WHERE f2.name = 'Big Woods') AS "bigwoods") AS "bigwoods_count"
+WHERE forest_no <> (SELECT f3.forest_no
+                    FROM FOREST f3
+                    WHERE f3.name = 'Big Woods')
+GROUP BY forest_no, bigwoods_count.count
+HAVING COUNT(forest_no) = bigwoods_count.count) AS "final_forest_no";
 
-----H
 
 
------------------------------------
----------QUESTION 4----------------
------------------------------------
 
-----a
+
+-------- Question 4
+--(a)
 DROP VIEW IF EXISTS DUTIES;
 CREATE OR REPLACE VIEW DUTIES AS
-    SELECT S.maintainer, count(S.sensor_id) AS num_sensors
-    FROM SENSOR S
-    WHERE S.maintainer IS NOT NULL
-    GROUP BY S.maintainer;
+SELECT maintainer, COUNT(sensor_id) AS num_sensors
+FROM SENSOR
+WHERE maintainer IS NOT NULL
+GROUP BY maintainer;
 
----B
+--(b)
 DROP MATERIALIZED VIEW IF EXISTS DUTIES_MV;
 CREATE MATERIALIZED VIEW DUTIES_MV AS
-    SELECT S.maintainer, count(S.sensor_id) AS num_sensors
-    FROM SENSOR S
-    WHERE S.maintainer IS NOT NULL
-    GROUP BY S.maintainer;
+SELECT maintainer, COUNT(sensor_id) AS num_sensors_mv
+FROM SENSOR
+WHERE maintainer IS NOT NULL
+GROUP BY maintainer;
 
-----C
+--(c)
 DROP VIEW IF EXISTS FOREST_SENSOR;
 CREATE OR REPLACE VIEW FOREST_SENSOR AS
-    SELECT F.forest_no, s.sensor_id
-    FROM FOREST F
-         JOIN SENSOR S ON S.x BETWEEN F.mbr_xmin AND F.mbr_xmax AND S.y BETWEEN F.mbr_ymin AND F.mbr_ymax;
+SELECT f.forest_no, f.name, s.sensor_id
+FROM FOREST f, SENSOR s
+WHERE s.x BETWEEN f.mbr_xmin AND f.mbr_xmax AND s.y BETWEEN f.mbr_ymin AND f.mbr_ymax;
 
-----D
+--(d)
 DROP VIEW IF EXISTS FOREST_ROAD;
 CREATE OR REPLACE VIEW FOREST_ROAD AS
-    SELECT forest_no, COUNT(road_no)
-    FROM intersection
-    GROUP BY forest_no;
+SELECT forest_no, COUNT(road_no) as count
+FROM INTERSECTION
+GROUP BY forest_no;
+
+
+-------- Question 5
+REFRESH MATERIALIZED VIEW DUTIES_MV;
+--(a)
+
+SELECT forest_no
+FROM (SELECT forest_no, RANK() over (ORDER BY count DESC) as rank
+       FROM FOREST_ROAD) AS rank_view
+WHERE rank = 2;
+
+--(b)
+SELECT maintainer, name, employing_state, area
+FROM COVERAGE C JOIN (SELECT maintainer, name, employing_state
+                        FROM (SELECT maintainer, RANK() OVER (ORDER BY num_sensors DESC) as rank
+                                FROM DUTIES) AS workers
+                        JOIN WORKER w on workers.maintainer = w.ssn
+                        WHERE workers.rank = 1) AS get_state on get_state.employing_state = c.state;
+
+--(c)
+
+SELECT F.name
+FROM FOREST_SENSOR AS F
+WHERE F.NAME NOT IN (SELECT F2.name
+                        FROM REPORT R JOIN FOREST_SENSOR F2 on R.sensor_id = F2.sensor_id
+                        WHERE R.report_time BETWEEN '2020-10-10 00:00:00' AND '2020-10-11 00:00:00');
+--(d)
+
+SELECT maintainer, name, employing_state, area
+FROM COVERAGE C JOIN (SELECT maintainer, name, employing_state
+                        FROM (SELECT maintainer, RANK() OVER (ORDER BY num_sensors_mv DESC) as rank
+                                FROM DUTIES_MV) AS workers
+                        JOIN WORKER w on workers.maintainer = w.ssn
+                        WHERE workers.rank = 1) AS get_state on get_state.employing_state = c.state;
+
+--(e)
+---i really dont understand the question
+
 
