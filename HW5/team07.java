b545121b85c113490133cc8e24292496355ce779
Sushruti Bansod <60163688+sbansod21@users.noreply.github.com>
2021-11-29 22:32:40 -0500

    Add files via upload

--- HW5/team07.java
+++ HW5/team07.java
@@ -32,11 +32,11 @@ public class team07 {
                 case 4:
                     team07.switchWorkers(conn);
                     continue;
-                case 5:
-                    team07.updateSensorStatus(conn);
-                    continue;
-                case 6:
-                    team07.updateForestArea(conn);
+//                case 5:
+//                    team07.updateSensorStatus(conn);
+//                    continue;
+//                case 6:
+//                    team07.updateForestArea(conn);
             }
         }
     }
@@ -49,8 +49,8 @@ public class team07 {
         System.out.println("2: Add Worker");
         System.out.println("3: Add Sensor");
         System.out.println("4: Switch Workers Duties");
-        System.out.println("5: Update Sensor Status");
-        System.out.println("6: Update Forest Area Covered");
+//        System.out.println("5: Update Sensor Status");
+//        System.out.println("6: Update Forest Area Covered");
 
 
         return inScan.nextInt();
@@ -62,13 +62,12 @@ public class team07 {
         String forestName = "";
         double area, acidLevel = 0, xmin = 0, xmax = 0, ymin = 0, ymax = 0;
         boolean validName = false;
-
+        inScan.nextLine();
         try{
             ///getting the input from the user and checking integrity statements
             while (!validName) {
                 System.out.print("Please enter the forest name: ");
                 forestName = inScan.nextLine();
-                forestName = forestName.toUpperCase();
 
                 String compareName = "SELECT name FROM FOREST WHERE name = '"+forestName+"'";
                 ResultSet res1 = st.executeQuery(compareName);
@@ -156,9 +155,9 @@ public class team07 {
     private static void insertWorker(Connection conn) throws SQLException
     {
         int rank;
-        String name = "", ssn = "", employingState;
+        String name = "", ssn = "", employingState = " ";
         Statement st = conn.createStatement();
-
+        inScan.nextLine();
         try {  
             boolean validSsn = false;
             while (!validSsn) 
@@ -180,7 +179,6 @@ public class team07 {
             {
                 System.out.print("Please enter the worker's name: ");
                 name = inScan.nextLine();
-                name = name.toUpperCase();
 
                 String query2 = "SELECT name FROM WORKER WHERE name = '" + name + "'";
                 ResultSet res2 = st.executeQuery(query2);
@@ -194,11 +192,21 @@ public class team07 {
             rank = inScan.nextInt();
 
             inScan.nextLine();
+            boolean validState = false;
+            while (!validState) {
+                System.out.print("Please enter the worker's employing state: ");
+                employingState = inScan.nextLine();
 
-            System.out.print("Please enter the worker's employing state: ");
-            employingState = inScan.nextLine();
+                if(employingState.length() == 2)
+                {
+                    validState = true;
+                }else
+                {
+                    System.out.println("State abbreviation invalid. Please enter the state abbreviation (PA, OH).");
+                }
+            }
 
-            //    conn.setAutoCommit(true);
+             conn.setAutoCommit(false);
             //add a method that checks if the state exists and if it does not
             //insert it into the table
             String insert = "INSERT INTO WORKER VALUES ('" + ssn + "', '" + name + "', " + rank + ", '" + employingState + "');";
@@ -231,4 +239,198 @@ public class team07 {
         }
     }
 
+    private static void insertSensor(Connection conn) throws SQLException
+    {
+        double x = 0, y = 0;
+        String maintainer = "";
+        Statement st = conn.createStatement();
+        inScan.nextLine();
+        try {
+            boolean validCord = false;
+            while (!validCord) {
+                System.out.print("Please enter the x coordinate of the sensor: ");
+                x = inScan.nextDouble();
+                System.out.print("Please enter the y coordinate of the sensor: ");
+                y = inScan.nextDouble();
+
+                String query1 = "SELECT x,y FROM SENSOR WHERE x = '" + x + "' AND y = '" + y + "'";
+                ResultSet res1 = st.executeQuery(query1);
+
+                if (!res1.next())
+                    validCord = true;
+                else
+                    System.out.print("The x, y coordinates already exist. ");
+            }
+            inScan.nextLine();
+            boolean validName = false;
+            while(!validName) {
+                System.out.print("Please enter the maintainer's Social Security Number: ");
+                maintainer = inScan.nextLine();
+
+                String query3 = "SELECT ssn FROM WORKER WHERE ssn = '"+maintainer+"'";
+                ResultSet res3 = st.executeQuery(query3);
+
+                if(res3.next())
+                    validName = true;
+                else
+                    System.out.print("The ssn is not in the worker's table. ");
+            }
+
+            System.out.println("Enter the time the sensor was last charged:");
+            System.out.println("Enter the month");
+            String lc_month= inScan.next();
+            System.out.println("Enter the day");
+            String lc_day= inScan.next();
+            System.out.println("Enter the year");
+            String lc_year= inScan.next();
+            System.out.println("Enter the time (hh24:mi)");
+            String lc_time= inScan.next();
+
+            System.out.println("Enter the time the sensor was last read:");
+            System.out.println("Enter the month");
+            String lr_month= inScan.next();
+            System.out.println("Enter the day");
+            String lr_day= inScan.next();
+            System.out.println("Enter the year");
+            String lr_year= inScan.next();
+            System.out.println("Enter the time (hh24:mi)");
+            String lr_time= inScan.next();
+
+            System.out.println("Enter the energy level of the sensor:");
+            int energy_level = inScan.nextInt();
+
+            String last_charged = lc_month + "/" + lc_day + "/" + lc_year + " " + lc_time;
+            String last_read = lr_month + "/" + lr_day + "/" + lr_year + " " + lr_time;
+
+            String insert = String.format("INSERT INTO SENSOR(x,y,last_charged,maintainer, last_read,energy) VALUES (%f,%f,TO_TIMESTAMP('%s', 'mm/dd/yyyy hh24:mi'),\'" +
+                    "%s',TO_TIMESTAMP('%s', 'mm/dd/yyyy hh24:mi'),%d);", x,y,last_charged,maintainer, last_read, energy_level);
+            conn.setAutoCommit(false);
+            PreparedStatement insertWorker = conn.prepareStatement(insert);
+            conn.commit();
+
+            PreparedStatement query2 = conn.prepareStatement("SELECT sensor_id FROM SENSOR WHERE x = ? AND y = ?");
+            query2.setDouble(1, x);
+            query2.setDouble(2, y);
+            ResultSet res2 = query2.executeQuery();
+            while(res2.next()) {
+                int confirmNum = res2.getInt("sensor_id");
+                System.out.printf("\nYour insertion is successful with sensor id %d\n", confirmNum);
+            }
+        }
+        catch (SQLException e1) {
+            try {
+                System.out.print(e1.toString());
+                System.out.println("The current insertion failed.");
+                String query2 = "SELECT setval('SENSOR_sensor_id_seq', MAX(sensor_id), true) FROM SENSOR;";
+                st.executeQuery(query2);
+                conn.rollback();
+            } catch (SQLException e2) {
+                System.out.println(e2.toString());
+            }
+        }
+        catch (Exception e){
+            System.out.println(e.toString());
+            System.out.println("Then input is invalid.");
+            System.exit(0);
+        }
+    }
+
+    private static void switchWorkers(Connection conn) throws SQLException
+    {
+        String nameA = "", nameB = "";
+        String ssnA = "", ssnB = "";
+        Statement st = conn.createStatement();
+        inScan.nextLine();
+        boolean validNameA = false;
+        while(!validNameA){
+            System.out.print("Enter the name of worker A: ");
+            nameA = inScan.nextLine();
+
+            String query1 = "SELECT ssn, name FROM WORKER WHERE name = '"+nameA+"'";
+            ResultSet res1 = st.executeQuery(query1);
+
+            if(res1.next())
+            {
+                validNameA = true;
+                ssnA = res1.getString(1);
+            }
+            else
+                System.out.print("Worker A's name is not in the worker table. ");
+        }
+        boolean validNameB = false;
+        while(!validNameB){
+            System.out.print("Enter the name of worker B: ");
+            nameB = inScan.nextLine();
+
+            String query2 = "SELECT ssn,name FROM WORKER WHERE name = '"+nameB+"'";
+            ResultSet res2 = st.executeQuery(query2);
+
+            if(res2.next())
+            {
+                validNameB = true;
+                ssnB = res2.getString(1);
+            }
+
+            else
+                System.out.print("Worker B's name is not in the worker table. ");
+        }
+
+        Boolean rReturn;
+        CallableStatement properCase = conn.prepareCall("{ ? = call checkState( ?, ? ) }");
+        properCase.registerOutParameter(1, Types.BIT);
+        properCase.setString(2, nameA);
+        properCase.setString(3, nameB);
+        properCase.execute();
+        rReturn = properCase.getBoolean(1);
+        properCase.close();
+        if(!rReturn)
+        {
+            System.out.println("The workers are not eligible to work in the others state.");
+            return;
+        }
+
+        try{
+            conn.setAutoCommit(false);
+            String insertHolder = "INSERT INTO WORKER VALUES ('xxxxxxxxx','x',0,'PA');";
+            st.executeUpdate(insertHolder);
+            conn.commit();
+
+            PreparedStatement update1 = conn.prepareStatement("UPDATE SENSOR SET maintainer = 'xxxxxxxxx' WHERE maintainer = ? ;");
+            update1.setString(1, ssnA);
+            update1.executeUpdate();
+            conn.commit();
+
+            PreparedStatement update2 = conn.prepareStatement("UPDATE SENSOR SET maintainer = ? WHERE maintainer = ? ;");
+            update2.setString(1, ssnA);
+            update2.setString(2, ssnB);
+            update2.executeUpdate();
+            conn.commit();
+
+            PreparedStatement update3 = conn.prepareStatement("UPDATE SENSOR SET maintainer = ? WHERE maintainer = 'xxxxxxxxx' ;");
+            update3.setString(1, ssnB);
+            update3.executeUpdate();
+            conn.commit();
+
+            String deleteHolder = "DELETE FROM WORKER WHERE ssn='xxxxxxxxx';";
+            st.executeUpdate(deleteHolder);
+            conn.commit();
+
+            System.out.println("Your switch is successful.");
+        }
+        catch (SQLException e1) {
+            try {
+                System.out.print(e1.toString());
+                System.out.println("The switch failed.");
+                conn.rollback();
+            } catch (SQLException e2) {
+                System.out.println(e2.toString());
+            }
+        }
+        catch (Exception e){
+            System.out.println("Then input is invalid.");
+            System.exit(0);
+        }
+    }
+
+    private static void
 }
